88db9e57c811c663bcaa539901adb3cfb9927eaa
Phon-Seng <phonseng202@gmail.com>
2024-06-24 20:45:13 +0630

    Exercise4

---
+++ Exercise4_phon_s.py
@@ -0,0 +1,9 @@
+FE=75
+GB=67
+IC=80
+CP=85
+print("____Your Score____")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",IC)
+print("Computer Programming :",CP)
